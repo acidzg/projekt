66d8439236a4fc49bbe991dcbf06c6df7d61105b
Ponuras <adampankowski@gazeta.pl>
2014-06-11 11:39:30 +0200

    Dodany prosty GUI

--- Projekt Log Collector/src/ApplicationCore.java
+++ Projekt Log Collector/src/ApplicationCore.java
@@ -3,19 +3,20 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import javax.swing.JTextArea;
+
 public class ApplicationCore {
-	private final String configurationFile = "configuration.txt";
 	private Configuration configuration;
 	private QueueManager queue;
 	private InputFileAdapter inputFileAdapter;
 	private OutputDatabaseAdapter outputDatabaseAdapter;
 
-	public ApplicationCore() {
+	public ApplicationCore(JTextArea guiConsole) {
 		configuration = readAndCreateConfiguration();
 		//outputDatabaseAdapter = new OutputDatabaseAdapter();
 		queue = new QueueManager(outputDatabaseAdapter);
 		queue.start();
-		inputFileAdapter = new InputFileAdapter();
+		inputFileAdapter = new InputFileAdapter(guiConsole);
 		inputFileAdapter.start();
 		inputFileAdapter.connectToQueueManager(queue);
 		outputDatabaseAdapter.setupConfig(configuration);
