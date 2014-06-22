315aba4b29f00bc3ba6231ff3ffe49a4126823f3
whtvr666 <what.adams.ever@gmail.com>
2014-06-22 14:53:04 +0200

    Dodanie brakującego zamykania połączenia do funkcji zapisującej, drobne poprawki dodatkowe

--- Projekt Log Collector/src/OutputDatabaseAdapter.java
+++ Projekt Log Collector/src/OutputDatabaseAdapter.java
@@ -1,6 +1,7 @@
+
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
+//import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
@@ -12,34 +13,38 @@ public class OutputDatabaseAdapter implements OutputAdapter {
 	// /statement does not appear
 	private Statement statement = null;
 	// /wynik zapytania sqlQuery
-	private ResultSet resultSet;
-
+	//private ResultSet resultSet;
+	// ^ ta zmienna chyba nie potrzebna?
 	// /query of sql
 
 	public OutputDatabaseAdapter() {
 	}
 
-	public void setupConfig(Configuration config) {
+	public final void setupConfig(final Configuration config) {
 		this.configuration = config;
 	}
 
-	public boolean storeEvents(List<Event> batch) {
+	public final boolean storeEvents(final List<Event> batch) {
 		boolean flag = false;
 		if (openConnection()) {
 			for (Event event : batch) {
-				flag = statement.execute("INSERT INTO Events VALUES ("
-						+ event.timestamp + ", " + event.loglevel + ", "
+				flag = statement.execute(
+						"INSERT INTO Events VALUES ("
+						+ event.timestamp
+						+ ", " + event.loglevel + ", "
 						+ event.details + ");");
-				if (!flag)
+				if (!flag) {
 					break;
+				}
 			}
 		}
+		closeConnection();
 		return flag;
 	}
 
 	private boolean openConnection() {
 		try {
-			Class.forName("com.mysql.jdbc.Driver"); // loading mysql driver
+			Class.forName("com.mysql.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
@@ -49,10 +54,12 @@ public class OutputDatabaseAdapter implements OutputAdapter {
 					configuration.outConf.databaseLogin,
 					configuration.outConf.databasePass);
 			statement = connection.createStatement();
-			System.out.println("nawiazano polaczenie z baza danych");
+			System.out.println(
+					"nawiazano polaczenie z baza danych");
 			return true;
 			// connection =
-			// DriverManager.getConnection("jdbc:mysql://matuszak.net.pl",
+			// DriverManager.getConnection
+			//("jdbc:mysql://matuszak.net.pl",
 			// "matuszak_abc", "1qaz2wsx");
 			// getting connection to database
 		} catch (SQLException e) {
