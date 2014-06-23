0dfbccb154d61f4d83392ef8f108bdb84372f169
Mateusz Czaja <matoxc@gmail.com>
2014-06-23 19:18:26 +0200

    Update OutputAdapter.java

--- Projekt Log Collector/src/OutputAdapter.java
+++ Projekt Log Collector/src/OutputAdapter.java
@@ -2,8 +2,5 @@ import java.util.List;
 
 public abstract interface OutputAdapter {
 	public abstract void setupConfig(Configuration configuration);
-	
-	public abstract boolean openConnection();
 	public abstract boolean storeEvents(List<Event> list);
-	public abstract void closeConnection();
 }
