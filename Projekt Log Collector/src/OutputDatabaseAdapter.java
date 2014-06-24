f7774a929311290430a20ac492ba6d2a3fb376d7
whtvr666 <what.adams.ever@gmail.com>
2014-06-24 11:54:44 +0200

    Poprawki w związku z błędnym działaniem

--- Projekt Log Collector/src/OutputDatabaseAdapter.java
+++ Projekt Log Collector/src/OutputDatabaseAdapter.java
@@ -1,90 +1,73 @@
-
 import java.sql.Connection;
 import java.sql.DriverManager;
-//import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 
 public class OutputDatabaseAdapter implements OutputAdapter {
 
-	private Configuration configuration;
-	private Connection connection = null;
-	// /statement does not appear
-	private Statement statement = null;
-	// /wynik zapytania sqlQuery
-	//private ResultSet resultSet;
-	// ^ ta zmienna chyba nie potrzebna?
-	// /query of sql
+    private Configuration configuration;
+    private Connection connection = null;
+    // /statement does not appear
+    private Statement statement = null;
 
-	public OutputDatabaseAdapter() {
-	}
+    public OutputDatabaseAdapter() {
+    }
 
-	public final void setupConfig(final Configuration config) {
-		this.configuration = config;
-	}
+    public final void setupConfig(final Configuration config) {
+        this.configuration = config;
+    }
 
-	public synchronized final boolean storeEvents(final List<Event> batch) {
-		boolean flag = false;
-		if (openConnection()) {
-			for (Event event : batch) {
-				try {
-					flag = statement.execute(
-							 
-							 "INSERT INTO project202idz.events(timestamp, level, details) VALUES (\'"
-							+ event.getTimestamp()
-							+ "\', \'" + event.getLogLevel() + "\', \'"
-							+ event.getDetails() + "\');");
-					
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				if (!flag) {
-					break;
-				}
-			}
-		}
-		closeConnection();
-		return flag;
-	}
+    public final synchronized boolean storeEvents(final List<Event> batch) {
+        boolean flag = true;
+        if (openConnection()) {
+            for (Event event : batch) {
+                try {
+                    flag = statement.execute(
+                    		"INSERT INTO project202idz.events("
+                            + "timestamp, level, details) VALUES (\'"
+                            + event.getTimestamp()
+                            + "\', \'" + event.getLogLevel() + "\', \'"
+                            + event.getDetails() + "\');");
+                    
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                if (flag) {
+                    break;
+                }
+            }
+        }
+        closeConnection();
+        return flag;
+    }
 
-	private boolean openConnection() {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		try {
-			connection = DriverManager.getConnection(
-					configuration.outConf.databaseAddress,
-					configuration.outConf.databaseLogin,
-					configuration.outConf.databasePass);
-			statement = connection.createStatement();
-			System.out.println(
-					"nawiazano polaczenie z baza danych");
-			return true;
-			// connection =
-			// DriverManager.getConnection
-			//("jdbc:mysql://matuszak.net.pl",
-			// "matuszak_abc", "1qaz2wsx");
-			// getting connection to database
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return false;
-	}
+    private boolean openConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            connection = DriverManager.getConnection(
+                    configuration.outConf.databaseAddress,
+                    configuration.outConf.databaseLogin,
+                    configuration.outConf.databasePass);
+            statement = connection.createStatement();
+            System.out.println(
+                    "nawiazano polaczenie z baza danych");
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 
-	private void closeConnection() {
-		try {
-			connection.close(); // closing a current connection
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-	// public static void main(String args[]){
-	// OutputDatabaseAdapter adapter = new OutputDatabaseAdapter();
-	// adapter.openConnection();
-	// }
-	
+    private void closeConnection() {
+        try {
+            connection.close(); // closing a current connection
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
